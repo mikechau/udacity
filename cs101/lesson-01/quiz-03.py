a8ca1a58d910aa630c4a645b4b8176c44546a5ac
Mike Chau <mchau@iwu.edu>
2013-05-22 01:10:59 -0500

    Initial commit

---
+++ cs101/lesson-01/quiz-03.py
@@ -0,0 +1,14 @@
+# Given the variables defined here, write Python 
+# code that prints out the distance, in meters, 
+# that light travels in one processor cycle. 
+
+speed_of_light = 299792458.0
+cycles_per_second = 2700000000.0
+
+cycle_distance = speed_of_light / cycles_per_seconds
+
+# 0.111034243704
+
+print cycle_distance
+
+print cycle_distance * 100 # cycles in cm
